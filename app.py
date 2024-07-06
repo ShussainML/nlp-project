a74235ff3a7c942c2d2a3c78b61bee88cf57898b
Sajid Hussain <sajid.hussain@conduitly.com>
2024-07-06 17:01:00 +0500

    up

--- app.py
+++ app.py
@@ -10,19 +10,22 @@ def download_from_drive(file_id, output):
     gdown.download(url, output, quiet=False)
 
 # Function to load model and tokenizer
-@st.cache_resource()
-def load_model_and_tokenizer(model_dir):
+@st.cache_resource(allow_output_mutation=True)
+def load_model_and_tokenizer():
+    model_dir = "model"  # Directory to store the downloaded model
+    
     # Ensure the model directory exists
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
     
     # Google Drive file IDs
-    model_file_id = "1sg5Bh9mNzFTPLQ6lJ7sdLysGfvsc_QCk"  # ID for model.safetensors or pytorch_model.bin
+    model_file_id = "1sg5Bh9mNzFTPLQ6lJ7sdLysGfvsc_QCk"  # ID for pytorch_model.bin
     config_file_id = "1fG6ipwD1beP0T3Sg3a8mWZMS2RPhuV5I"  # ID for config.json
     special_tokens_file_id = "1C0AKDHXgy3t5XiWRm-F2bKlgQLEFns8d"  # ID for special_tokens_map.json
     tokenizer_config_file_id = "1_DzOqJxYt8PXwePOcvqbiXSAZVStpAMC"  # ID for tokenizer_config.json
     vocab_file_id = "1pM8C97I8SH3UVfPsUTBJsA_9xv8FeCJi"  # ID for vocab.json
     merges_file_id = "1HlrLg_CuoGFsPKHEPTYOEw1ZK79QfqWI"  # ID for merges.txt
+    
     # Paths to save the files
     model_path = os.path.join(model_dir, "pytorch_model.bin")
     config_path = os.path.join(model_dir, "config.json")
@@ -30,6 +33,7 @@ def load_model_and_tokenizer(model_dir):
     tokenizer_config_path = os.path.join(model_dir, "tokenizer_config.json")
     vocab_path = os.path.join(model_dir, "vocab.json")
     merges_path = os.path.join(model_dir, "merges.txt")
+    
     # Download files
     download_from_drive(model_file_id, model_path)
     download_from_drive(config_file_id, config_path)
@@ -37,11 +41,15 @@ def load_model_and_tokenizer(model_dir):
     download_from_drive(tokenizer_config_file_id, tokenizer_config_path)
     download_from_drive(vocab_file_id, vocab_path)
     download_from_drive(merges_file_id, merges_path)
+    
     # Load tokenizer
     tokenizer = BartTokenizer.from_pretrained(model_dir)
     
     # Load model
-    model = BartForSequenceClassification.from_pretrained(model_dir)
+    try:
+        model = BartForSequenceClassification.from_pretrained(model_dir)
+    except OSError:
+        model = BartForSequenceClassification.from_pretrained(model_dir, from_tf=True)
     
     return model, tokenizer
 
@@ -66,8 +74,8 @@ def predict(model, tokenizer, text):
 def main():
     st.title("BART Model Deployment")
     
-    model_dir = "model"  # Directory to store the downloaded model
-    model, tokenizer = load_model_and_tokenizer(model_dir)
+    # Load model and tokenizer using st.cache_resource
+    model, tokenizer = load_model_and_tokenizer()
     
     text_input = st.text_input("Enter text to classify:", "")
     if st.button("Predict"):
